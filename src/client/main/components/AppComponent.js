252cf04caa33d0750b0779dcbc564dba6241596b
bloop <jaroslavkalinin97@gmail.com>
2020-07-20 17:26:45 +0300

    React-redux delpoyment finished

--- src/client/main/components/AppComponent.js
+++ src/client/main/components/AppComponent.js
@@ -1,14 +1,31 @@
-import React, { Suspense, useState } from 'react';
-import Main from './MainComponent.js';
-import SideMenu from './SideMenu/SideMenuComponent.js';
+import React, { Suspense, useEffect } from 'react';
 import { hot } from 'react-hot-loader/root';
+import { Provider } from 'react-redux';
+import store from '../redux/configureStore';
+import * as Actions from '../redux/ActionTypes';
+import { email, password } from '../shared/user';
+
+import Spinner from './Spinner/SpinnerComponent';
+import StoreWrapper from './StoreWrapperComponent';
+
 
 function App() {
+
+    //emulates auth process
+    useEffect(() => {
+        store.dispatch({type: Actions.AuthUser, email: email, password: password });
+    }, []);
+
     return (
-        <>
-            <SideMenu/>
-            <Main/>
-        </>
+        <Suspense fallback={
+            <Spinner>
+                Загрузка приложения...
+            </Spinner>
+        }>
+            <Provider store={store}>
+                <StoreWrapper/>
+            </Provider>
+        </Suspense>
     )
 }
 
--- src/client/main/components/Auth/AuthComponent.js
+++ src/client/main/components/Auth/AuthComponent.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
 import avatar from './avatar.png';
 
-export default function Auth(){
+const mapStateToProps = (state) => {
+    return {
+        email: state.user.email
+    }
+}
+
+function Auth(props){
     return (
         <div className="main__auth">
             <i className="far fa-bell main__auth-notify"></i>
             <img src={avatar} className="main__auth-avatar"/> 
-            <span className="main__auth-username">hello@giraffe.com</span>
+            <span className="main__auth-username">{ props.email }</span>
         </div>
     );
 }
+
+export default connect(mapStateToProps)(Auth);
